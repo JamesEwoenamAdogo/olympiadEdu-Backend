9bc0c4a524ecf8cf047bb262031f80e392bb9657
JamesEwoenamAdogo <jamesewoenam7@gmail.com>
2025-01-28 15:16:16 +0000

    purpose

--- Route/userRoute.js
+++ Route/userRoute.js
@@ -7,7 +7,7 @@ export const userRoute = express.Router()
 userRoute.post("/register-user",addUser)
 userRoute.post("/login", loginUser)
 userRoute.put("/update-user",authenticateUser, updateUsers)
-userRoute.get("/fetch-user-details",authenticateUser,updateAddInvoiceAddOns)
+userRoute.post("/fetch-user-details",authenticateUser,updateAddInvoiceAddOns)
 
 
 
