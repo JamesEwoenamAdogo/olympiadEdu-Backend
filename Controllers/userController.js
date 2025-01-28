ea269e4f53ce61ac93ace5c5d2a6e03e89535dc7
JamesEwoenamAdogo <jamesewoenam7@gmail.com>
2025-01-28 05:44:45 +0000

    purpose

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -14,6 +14,7 @@ export const addUser = async(req,res)=>{
         const {firstName, lastName,DOB,email,password,mobileNumber,Category,educationalLevel,grade,purpose_Of_Registration,gender,School,country}= req.body
         console.log(firstName,lastName,DOB, email,password,mobileNumber,Category,educationalLevel,grade,country,School,gender,purpose_Of_Registration)
         const existingUser = await userModel.find({email})
+        const allCompetitions = await competitionsSchema.find({})
         const exsitingUserByPhoneNumber = await userModel.find({mobileNumber})
         
         if(existingUser.length==1){
@@ -23,14 +24,25 @@ export const addUser = async(req,res)=>{
         if(exsitingUserByPhoneNumber.length==1){
             return res.json({success:false,message:"Phone Number already registered"})
         }
+
+        let userExams= []
+        for(let exam of purpose_Of_Registration){
+            let examinations = allCompetitions.filter((item)=>{return item.name == exam})
+            userExams.push(examinations)
+
+        }
         const hashedPassword = await bcrypt.hash(password,10)
         const userName = email.split("@")[0]+ Math.ceil(Math.random()*1000000)
-        const newUser= new userModel({firstName,lastName,DOB,email,password:hashedPassword,mobileNumber,Category,educationalLevel,grade,purpose_Of_Registration,userName,gender,School,country})
+        const newUser= new userModel({firstName,lastName,DOB,email,password:hashedPassword,mobileNumber,Category,educationalLevel,grade,purpose_Of_Registration:userExams,userName,gender,School,country})
+
+
+
+      
 
         newUser.save()
         const token = jwt.sign({id:newUser._id,firstName:newUser.firstName, lastName:newUser.lastName,userName:newUser.userName,Registered:newUser.Registered,Paid:newUser.Paid, Invoice:newUser.Invoice},process.env.TOKEN_SECRET, {expiresIn:"30m"})
-        
-        return res.json({success:true, message:"User Created successfully",token,userName,purpose_Of_Registration})
+       
+        return res.json({success:true, message:"User Created successfully",token,userName,purpose_Of_Registration:userExams})
 
 
 
