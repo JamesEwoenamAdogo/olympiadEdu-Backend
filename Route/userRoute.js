1ae87cbc64f07b344de897da167b8acef2b7acec
JamesEwoenamAdogo <jamesewoenam7@gmail.com>
2025-01-31 11:31:13 +0000

    Update userRoute.js

--- Route/userRoute.js
+++ Route/userRoute.js
@@ -1,11 +1,13 @@
 import express from "express"
-import { addUser, loginUser, updateUsers,updateAddInvoiceAddOns} from "../Controllers/userController.js"
+import { addUser, loginUser, updateUsers,updateAddInvoiceAddOns,loadPurposes} from "../Controllers/userController.js"
 import { authenticateUser } from "../Middlewares/authUser.js"
 
 export const userRoute = express.Router()
 
 userRoute.post("/register-user",addUser)
 userRoute.post("/login", loginUser)
+
+userRoutes.get("/load-purpose",authenticateUser,loadPurposes)
 userRoute.put("/update-user",authenticateUser, updateUsers)
 userRoute.post("/update-user-details",authenticateUser,updateAddInvoiceAddOns)
 
