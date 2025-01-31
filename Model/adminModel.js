a9c99c1566aaa52c01f85977c83c968b3efcf324
JamesEwoenamAdogo <jamesewoenam7@gmail.com>
2025-01-31 03:49:05 +0000

    Update adminModel.js

--- Model/adminModel.js
+++ Model/adminModel.js
@@ -1,1 +1,14 @@
+import mongoose from "mongoose";
 
+
+const adminModel = new mongoose.Schema({
+   
+    email:{
+        type:String
+    },
+    password:{
+        type:String
+    }
+},{timestamps:true})
+
+export const adminSchema = mongoose.model("admin", adminModel)
