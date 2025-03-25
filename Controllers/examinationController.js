ef8edf1871ff1b4a881b6dc1871c44b419c104c2
JamesEwoenamAdogo <jamesewoenam7@gmail.com>
2025-03-25 12:03:51 +0000

    Quiz

--- Controllers/examinationController.js
+++ Controllers/examinationController.js
@@ -1,11 +1,34 @@
-import { examinationModel } from "../Model/Examination";
+import { examinationModel } from "../Model/Examination.js";
+import cloudinary from "../utils/cloudinaryConfig.js";
+
+
 
 export const addExamination = async(req,res)=>{
     try{
-        const examBody = req.body 
-        const newExam = new examinationModel(examBody)
-        newExam.save()
-        return res.json({message:"success"})
+        const { title, description, time, numberOfQuestions, questions } = req.body;
+        console.log(req.body)
+
+        const parsedQuestions = JSON.parse(questions)
+
+        let publicId
+        
+        await cloudinary.uploader.upload(req.file.path, (err, result)=>{
+            if(err){
+                console.log(err)
+                return  res.status(500).json({sucess:false,message:"Error"})
+                
+            }
+            res.json({success:true,message:"Picture uploaded",data: result})
+           
+            publicId = result.public_id
+           
+
+        });
+
+
+        const newQuiz = new examinationModel({title,description,time,numberOfQuestions,questions:parsedQuestions,image:publicId})
+        newQuiz.save()
+        return res.json({success:true, message:"success"})
 
 
 
--- Route/examinationRoute.js
+++ Route/examinationRoute.js
@@ -1,7 +1,10 @@
 import express from "express"
 import { addExamination , allExam } from "../Controllers/examinationController"
+import { upload } from "../Middlewares/UploadFile"
 
 export const examRoute = express.Router()
 
-examRoute.post("/add-exam",addExamination)
+
+
+examRoute.post("/add-exam",upload.single("image"),addExamination)
 examRoute.post("/all-exam", allExam)
