e726f60c749e1eaa8ee56575bd7d89dd2257eadc
JamesEwoenamAdogo <jamesewoenam7@gmail.com>
2025-03-30 20:21:09 +0000

    Update googleCloudConfig.js

--- utils/googleCloudConfig.js
+++ utils/googleCloudConfig.js
@@ -2,7 +2,7 @@ import { Storage } from "@google-cloud/storage";
 import path from "path"
 
 import { fileURLToPath } from 'url';
-import path from 'path';
+// import path from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
