464505051b1f78b1a4e37d51a3f01760dc92d2e8
James Adogo Ewoenam <jamesewoenam7@gmail.com>
2025-04-11 12:49:12 +0000

    Update

--- Middlewares/authUser.js
+++ Middlewares/authUser.js
@@ -3,19 +3,25 @@ import dotenv from "dotenv"
 dotenv.config()
 
 export const authenticateUser = async(req,res,next)=>{
+    try{
+        const token = req.headers.token
+        console.log(token)
+        const verifyUser = jwt.verify(token,process.env.TOKEN_SECRET)
+        if(!token){
+            return res.json({success:false, message:"User not logged in"})
+        }
+        if(!verifyUser){
+            return res.json({success: false, message:"invalid token"})
+        }
 
-const token = req.headers.token
-console.log(token)
-const verifyUser = jwt.verify(token,process.env.TOKEN_SECRET)
-if(!token){
-    return res.json({success:false, message:"User not logged in"})
-}
-if(!verifyUser){
-    return res.json({success: false, message:"invalid token"})
-}
+        req.userId = verifyUser.id
+        next()
 
-req.userId = verifyUser.id
-next()
+    }catch(error){
+        console.log(error)
+    }
+    
+        
 
 
 }
